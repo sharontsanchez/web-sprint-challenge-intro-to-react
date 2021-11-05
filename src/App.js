2b788536db509faf9d844bc869ef2f1aed99e05c
Sharon Sanchez <sstharakan@gmail.com>
2021-11-05 12:35:50 -0500

    all tests passing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+
+// store and set the main data, star war characters will be retrieved from the axios request
+const [characterList, setCharacterList] = useState(null);
+
+// Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+useEffect(()=>{
+  axios
+  .get('https://swapi.dev/api/people')
+  .then (res => {
+    console.log(res);
+    setCharacterList(res.data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+},[]) // setting this to empty because it will run on initial render.
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters </h1>
+      {!characterList ? <h2>Loading...</h2> : characterList.map((character, index) => <Character key={index} character={character}/>)}
     </div>
   );
 }
 
 export default App;
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from "styled-components";
+
+const StyledH2 = styled.h2`
+color: #443e3e;
+text-shadow: 1px 1px 5px #fff;
+`;
+
+
+
+export default function Character({character}) {
+    return (
+        <StyledH2>{character.name}</StyledH2>
+    )
+}
+
